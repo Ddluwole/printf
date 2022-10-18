6352ae6ced9acaeeb407ca19d849cfd3ca661c99
David Ilori <92074984+Ddluwole@users.noreply.github.com>
2022-10-18 18:14:37 +0100

    Update base_converters.c

--- base_converters.c
+++ base_converters.c
@@ -6,14 +6,14 @@
 #include "main.h"
 
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags,int wid, int prec);
+		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output,
 		unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
 
 /**
  * convert_sbase - Converts a signed long to an inputted base and stores
- * the result to a buffer contained in a struct.
+ *                 the result to a buffer contained in a struct.
  * @output: A buffer_t struct containing a character array.
  * @num: A signed long to be converted.
  * @base: A pointer to a string containing the base to convert to.
@@ -35,7 +35,7 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 
 	if (num >= size || num <= -size)
 		ret += convert_sbase(output, num / size, base,
-				flags wid - 1, prec - 1);
+				flags, wid - 1, prec - 1);
 
 	else
 	{
@@ -57,16 +57,16 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 }
 
 /**
- * convert_ubase - Converts an unsigned long to an iputeed base and
- * stores the result to a buffer contained in a struct.
+ * convert_ubase - Converts an unsigned long to an inputted base and
+ *                 stores the result to a buffer contained in a struct.
  * @output: A buffer_t struct containing a character array.
  * @num: An unsigned long to be converted.
  * @base: A pointer to a string containing the base to convert to.
- * @flag: Flag modifiers.
+ * @flags: Flag modifiers.
  * @wid: A width modifier.
  * @prec: A precision modifier.
  *
- * Return: The number of bytes stored to he buffer.
+ * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec)
@@ -83,21 +83,21 @@ unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 
 	else
 	{
-		if (((flags >> 5) & 1) == 1) /* printing a ptr address */
+		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
 		{
 			wid -= 2;
 			prec -= 2;
 		}
-		for (; prec > 1; prec--, wid--) /*Handle precision */
+		for (; prec > 1; prec--, wid--) /* Handle precision */
 			ret += _memcpy(output, &pad, 1);
 
-		if (NEG_FLAG == 0) /* HAndle width */
+		if (NEG_FLAG == 0) /* Handle width */
 		{
 			pad = (ZERO_FLAG == 1) ? '0' : ' ';
 			for (; wid > 1; wid--)
 				ret += _memcpy(output, &pad, 1);
 		}
-		if (((flags >> 5) & 1) == 1) /* print 0x for ptr address */
+		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
 			ret += _memcpy(output, lead, 2);
 	}
 
